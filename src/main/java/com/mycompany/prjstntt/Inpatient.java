96e106f180fc1e40d9c61efc3c30f72189566aaa
Michael-cyber-create <bonjak408@gmail.com>
2026-09-08 18:03:41 +0000

    inpatient class update
    patient ids and credentials and other credentials and information strings update

--- src/main/java/com/mycompany/prjstntt/Inpatient.java
+++ src/main/java/com/mycompany/prjstntt/Inpatient.java
@@ -8,6 +8,57 @@ package com.mycompany.prjstntt;
  *
  * @author emeris
  */
-public class Inpatient {
-    
+public class Inpatient extends patient {
+
+    private String wardNumber;
+    private String bedNumber;
+
+    public Inpatient(String patientID,
+                     String firstName,
+                     String lastName,
+                     int age,
+                     String gender,
+                     String medicalCondition,
+                     PatientCategory category,
+                     String wardNumber,
+                     String bedNumber) {
+
+        super(patientID,
+              firstName,
+              lastName,
+              age,
+              gender,
+              medicalCondition,
+              category);
+
+        this.wardNumber = wardNumber;
+        this.bedNumber = bedNumber;
+    }
+
+    public String getWardNumber() {
+        return wardNumber;
+    }
+
+    public void setWardNumber(String wardNumber) {
+        this.wardNumber = wardNumber;
+    }
+
+    public String getBedNumber() {
+        return bedNumber;
+    }
+
+    public void setBedNumber(String bedNumber) {
+        this.bedNumber = bedNumber;
+    }
+
+    @Override
+    public void displayDetails() {
+
+        super.displayDetails();
+
+        System.out.println("Ward Number       : " + wardNumber);
+        System.out.println("Bed Number        : " + bedNumber);
+        System.out.println("--------------------------------------------");
+    }
 }
+
