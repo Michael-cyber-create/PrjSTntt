533b6c52a132b551c6a2256e682603b4f8984076
Michael-cyber-create <bonjak408@gmail.com>
2026-09-08 18:00:48 +0000

    patient class code
    all chekc and strings added to patient class

--- src/main/java/com/mycompany/prjstntt/patient.java
+++ src/main/java/com/mycompany/prjstntt/patient.java
@@ -9,5 +9,104 @@ package com.mycompany.prjstntt;
  * @author emeris
  */
 public class patient {
-    
+
+    private String patientID;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String gender;
+    private String medicalCondition;
+    private PatientCategory category;
+
+    public patient(String patientID, String firstName, String lastName,
+                   int age, String gender, String medicalCondition,
+                   PatientCategory category) {
+
+        this.patientID = patientID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.gender = gender;
+        this.medicalCondition = medicalCondition;
+        this.category = category;
+    }
+
+    public String getPatientID() {
+        return patientID;
+    }
+
+    public void setPatientID(String patientID) {
+        this.patientID = patientID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getMedicalCondition() {
+        return medicalCondition;
+    }
+
+    public void setMedicalCondition(String medicalCondition) {
+        this.medicalCondition = medicalCondition;
+    }
+
+    public PatientCategory getCategory() {
+        return category;
+    }
+
+    public void setCategory(PatientCategory category) {
+        this.category = category;
+    }
+
+    public void displayDetails() {
+
+        System.out.println("--------------------------------------------");
+        System.out.println("Patient ID        : " + patientID);
+        System.out.println("First Name        : " + firstName);
+        System.out.println("Last Name         : " + lastName);
+        System.out.println("Age               : " + age);
+        System.out.println("Gender            : " + gender);
+        System.out.println("Medical Condition : " + medicalCondition);
+        System.out.println("Category          : " + category);
+        System.out.println("--------------------------------------------");
+    }
+
+    @Override
+    public String toString() {
+
+        return patientID + " - "
+                + firstName + " "
+                + lastName + " - "
+                + category;
+    }
+} 
 }
