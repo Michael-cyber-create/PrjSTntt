a883aeeafffeed7d9aec57d1c20362ef3f369cba
Michael-cyber-create <bonjak408@gmail.com>
2026-09-08 17:37:49 +0000

    all classes

---
+++ src/main/java/com/mycompany/prjstntt/PrjSTntt.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prjstntt;
+
+/**
+ *
+ * @author emeris
+ */
+public class PrjSTntt {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
