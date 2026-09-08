3054290487ca641ba36dccea76e84465b9110f8e
Michael-cyber-create <bonjak408@gmail.com>
2026-09-08 18:06:15 +0000

    Bed class update
    beds count and info of beds and which patients occupy a bed / true or false

--- src/main/java/com/mycompany/prjstntt/Bed.java
+++ src/main/java/com/mycompany/prjstntt/Bed.java
@@ -9,5 +9,40 @@ package com.mycompany.prjstntt;
  * @author emeris
  */
 public class Bed {
-    
-}
+
+    private String bedNumber;
+    private boolean occupied;
+    private String patientID;
+
+    public Bed(String bedNumber) {
+
+        this.bedNumber = bedNumber;
+        this.occupied = false;
+        this.patientID = "";
+    }
+
+    public String getBedNumber() {
+        return bedNumber;
+    }
+
+    public boolean isOccupied() {
+        return occupied;
+    }
+
+    public String getPatientID() {
+        return patientID;
+    }
+
+    public void occupy(String patientID) {
+
+        occupied = true;
+        this.patientID = patientID;
+    }
+
+    public void release() {
+
+        occupied = false;
+        patientID = "";
+    }
+}   
+
